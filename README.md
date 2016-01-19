3ef96be5b91e2e88c61ee37733c777d52f7269b6
hm <harsham05@gmail.com>
2016-01-18 18:53:28 -0800

    working commit v2

---
+++ README.md
@@ -0,0 +1,28 @@
+
+## Edit Distance Similarity based on Metadata Values
+
+
+
+### Dependencies
+
+```
+    pip install editdistance
+```
+
+### Usage
+
+```
+Usage: edit-value-similarity.py [-h] --inputDir INPUTDIR --outCSV OUTCSV
+
+--inputDir INPUTDIR  path to directory containing files
+
+--outCSV OUTCSV      path to directory for storing the output CSV File, containing pair-wise edit distances
+
+```
+
+Eg: python edit-value-similarity.py --inputDir /path/to/files --outCSV /path/to/output.csv
+
+
+### License
+
+This project is licensed under the [Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
--- edit-value-similarity.py
+++ edit-value-similarity.py
@@ -1,6 +1,5 @@
 from tika import parser
-from pprint import pprint
-import os, editdistance, itertools, argparse
+import os, editdistance, itertools, argparse, csv
 
 def stringify(attribute_value):
     if isinstance(attribute_value, list):
@@ -14,77 +13,59 @@ if __name__ == "__main__":
     na_metadata = ["resourceName"]
     argParser = argparse.ArgumentParser('Edit Distance Similarity based on Metadata values')
     argParser.add_argument('--inputDir', required=True, help='path to directory containing files')
-    #argParser.add_argument('--outFile', required=True, help='path to directory for storing the output CSV File')
+    argParser.add_argument('--outCSV', required=True, help='path to directory for storing the output CSV File')
     args = argParser.parse_args()
 
-    if args.inputDir:# and args.outFile:
+    if args.inputDir and args.outCSV:
+        with open(args.outCSV, "wb") as outF:
+            a = csv.writer(outF, delimiter=',')
+            a.writerow(["x-coordinate","y-coordinate","edit_distance"])
 
-        #with open(args.outFile, "wb") as outF:
-        # a = csv.writer(delimiter=',')
+            filename_list = []
 
-        filename_list = []
+            for root, dirnames, files in os.walk(args.inputDir):
+                dirnames[:] = [d for d in dirnames if not d.startswith('.')]
+                for filename in files:
+                    if not filename.startswith('.'):
+                        filename_list.append(os.path.join(root, filename))
 
-        for root, dirnames, files in os.walk(args.inputDir):
-            dirnames[:] = [d for d in dirnames if not d.startswith('.')]
-            for filename in files:
-                if not filename.startswith('.'):
-                    filename_list.append(os.path.join(root, filename))
+            filename_list = [filename for filename in filename_list if parser.from_file(filename)]       #print "Debug, total files in directory", len(filename_list)
 
-        filename_list = [filename for filename in filename_list if parser.from_file(filename)]       #print "Debug, total files in directory", len(filename_list)
+            files_tuple = itertools.combinations(filename_list, 2)
+            for file1, file2 in files_tuple:
 
-        files_tuple = itertools.combinations(filename_list, 2)
-        for file1, file2 in files_tuple:
-            
-            print file1, file2
-            file1_parsedData = parser.from_file(file1)
-            file2_parsedData = parser.from_file(file2)
-    
+                row_edit_distance = []
+                row_edit_distance.append(file1)
+                row_edit_distance.append(file2)
 
-            intersect_features = set(file1_parsedData["metadata"].keys()) & set(file2_parsedData["metadata"].keys())
-    
-            intersect_features = [feature for feature in intersect_features if feature not in na_metadata ]
-    
-            print len(intersect_features)
-
-            file_edit_distance = 0.0
-
-            for feature in intersect_features:
-
-                file1_feature_value = stringify(file1_parsedData["metadata"][feature])
-                file2_feature_value = stringify(file2_parsedData["metadata"][feature])
-
-                print feature
-                print file1_feature_value
-                print file2_feature_value
-                              
-                feature_distance = float(editdistance.eval(file1_feature_value, file2_feature_value))/(len(file1_feature_value) if len(file1_feature_value) > len(file2_feature_value) else len(file2_feature_value))
-                print feature_distance, "\n\n"
-
-                file_edit_distance += feature_distance
-
-            file_edit_distance /= float(len(intersect_features))
-
-            print "File edit distance metric", (1-file_edit_distance)
+                file1_parsedData = parser.from_file(file1)
+                file2_parsedData = parser.from_file(file2)
         
+                intersect_features = set(file1_parsedData["metadata"].keys()) & set(file2_parsedData["metadata"].keys())
+                intersect_features = [feature for feature in intersect_features if feature not in na_metadata ]
+
+                file_edit_distance = 0.0
+                for feature in intersect_features:
+
+                    file1_feature_value = stringify(file1_parsedData["metadata"][feature])
+                    file2_feature_value = stringify(file2_parsedData["metadata"][feature])
+
+                    #print feature                print file1_feature_value                     print file2_feature_value
+                                  
+                    feature_distance = float(editdistance.eval(file1_feature_value, file2_feature_value))/(len(file1_feature_value) if len(file1_feature_value) > len(file2_feature_value) else len(file2_feature_value))
+                    #print feature_distance, "\n\n"
+
+                    file_edit_distance += feature_distance
+
+                file_edit_distance /= float(len(intersect_features))    #average edit distance
+
+                row_edit_distance.append(1-file_edit_distance)
+                a.writerow(row_edit_distance)
+
+                print "File edit distance metric", (1-file_edit_distance)
             
-            break
                 
-        
-    
-    
+                    
             
-
-
-
-
         
-
-
-
-        
-
-
-             
-        
-            
         
