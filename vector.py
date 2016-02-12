756e56a5aaed9f26691116ac66cd29fb7c660520
hm <harsham05@gmail.com>
2016-02-12 01:43:23 -0800

    add cosine distance

--- vector.py
+++ vector.py
@@ -1,37 +1,45 @@
 import math
 
+def stringify(attribute_value):
+    if isinstance(attribute_value, list):
+        return str((", ".join(attribute_value)).encode('utf-8').strip())
+    else:
+        return str(attribute_value.encode('utf-8').strip())
+
+
 class Vector:
     '''
     An instance of this class represents a vector in n-dimensional space
     '''
 
-    def __init__(self, vectorID, features):
+    def __init__(self, features):
         '''
         Create a vector
-        @param vectorID identifier for vector 
-        @param features 
+        @param  identifier for vector 
+        @param features => expect (string, int) key value pairs
         '''
-        self.vectorID = vectorID
-        self.dimensions = []
-        self.magnitudes = []
-        for dimension in features:
-            self.dimensions.append(dimension)
-            self.magnitudes.append(features[dimension])
+        na_metadata = ["resourceName"]
+        for na in na_metadata:
+            features.pop(na, None)
 
+        self.features = {}
+        for key in features:
+            self.features[key] = len(stringify(features[key]))
 
-    def __str__(self):
-        vector_str = "( {0} ): {\n".format(self.vectorID)
-        if this.dimensions and this.magnitudes:
-            for i in range(0, len(dimensions)):
-                vector_str += " {0}: {1} \n".format(dimensions[i], magnitudes[i])
-
-        return vector_str+"}\n"
-
+    
+    '''
+    def __str__(self):        
+        vector_str = "( {0} ): \n".format(self.)
+        if self.features:
+            for key in self.features:
+                vector_str += " {1}: {2} \n".format(key, self.features[key])
+        return vector_str+"\n"
+    '''
 
     def getMagnitude(self):
         totalMagnitude = 0.0
-        for magnitude in magnitudes:
-            totalMagnitude += magnitude ** 2
+        for key in self.features:
+            totalMagnitude += self.features[key] ** 2
         return math.sqrt(totalMagnitude)
 
 
@@ -40,16 +48,13 @@ class Vector:
         A = ax+by+cz
         B = mx+ny+oz
         A.B = a*m + b*n + c*o
-        '''
-
+        '''        
         dot_product = 0.0
-        intersect_features = set(self.dimensions) & set(anotherVector.dimensions)
-
+        intersect_features = set(self.features) & set(anotherVector.features)
+        
         for feature in intersect_features:
-            dot_product += self.magnitudes[feature] * anotherVector.magnitudes[feature]
-
+            dot_product += self.features[feature] * anotherVector.features[feature]
         return dot_product
-
 
 
     def cosTheta(self, v2):
@@ -57,7 +62,6 @@ class Vector:
         cosTheta = (V1.V2) / (|V1| |V2|)
         cos 0 = 1 implies identical documents
         '''
-        
         return self.dotProduct(v2) / (self.getMagnitude() * v2.getMagnitude())
 
 
